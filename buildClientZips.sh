7f4415985f07fc487698996a2fcea9c4176f7d91
Woody Christy <wchristy@cloudera.com>
2013-11-01 16:20:33 -0400

    Now with Pig!

--- buildClientZips.sh
+++ buildClientZips.sh
@@ -1,6 +1,6 @@
 while read line
 do
     versions=($line)
-     mvn clean install -Dcdh.version=${versions[1]} -Dmr.version=${versions[2]} -Dhive.version=${versions[3]}
+     mvn clean install -Dcdh.version=${versions[1]} -Dmr.version=${versions[2]} -Dhive.version=${versions[3]}  -Dpig.version=${versions[4]}
      zip -j ${versions[0]}.zip target/lib/*
 done <versions.txt
